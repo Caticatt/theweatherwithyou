94e497264eb4f73108a0cf3f1c07b0e5f30ac945
Alexandra Ferreira <alexandra.ferreira@singularbank.es>
2025-02-20 22:33:39 +0100

    Naviation modelated.

--- app/src/main/java/com/alexcfa/yourweather/ui/screens/Navigation.kt
+++ app/src/main/java/com/alexcfa/yourweather/ui/screens/Navigation.kt
@@ -9,22 +9,25 @@ import androidx.navigation.compose.rememberNavController
 import com.alexcfa.yourweather.ui.screens.hourly.HourlyScreen
 import com.alexcfa.yourweather.ui.screens.current.CurrentScreen
 
+
+
+sealed class NavScreen(val route : String){
+    data object CurrentScreen : NavScreen("currentScreen")
+    data object HourlyScreen : NavScreen("hourlyScreen")
+}
+
 @RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun Navigation() {
     val navController = rememberNavController()
-
-    NavHost(navController = navController, startDestination = "currentScreen") {
-        composable("currentScreen") {
+    NavHost(navController = navController, startDestination = NavScreen.CurrentScreen.route) {
+        composable(NavScreen.CurrentScreen.route) {
             CurrentScreen(onHourlyClick = { navController.navigate("hourly") })
         }
-
-        composable("hourly") {
+        composable(NavScreen.HourlyScreen.route) {
             HourlyScreen(
                 onBack = { navController.popBackStack() }
             )
         }
-
     }
-
 }
