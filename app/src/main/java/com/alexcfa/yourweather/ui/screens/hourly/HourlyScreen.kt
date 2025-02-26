d7aafe7a82d5b57255242193e85ef16b729673a1
Alexandra Ferreira <alexandra.ferreira@singularbank.es>
2025-02-26 23:07:17 +0100

    Added floating button and navigation refactoring.

--- app/src/main/java/com/alexcfa/yourweather/ui/screens/hourly/HourlyScreen.kt
+++ app/src/main/java/com/alexcfa/yourweather/ui/screens/hourly/HourlyScreen.kt
@@ -17,18 +17,24 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -53,7 +59,19 @@ fun HourlyScreen(
     viewModel: HourlyViewModel = viewModel()
 ) {
     Screen {
+
         val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior()
+        val state by viewModel.state.collectAsState()
+        val snackBarHostState = remember { SnackbarHostState() }
+
+        LaunchedEffect(state.message) {
+            state.message?.let {
+                snackBarHostState.currentSnackbarData?.dismiss()
+                snackBarHostState.showSnackbar(it)
+                viewModel.onMessageShown()
+            }
+
+        }
         Scaffold(
             topBar = {
                 val topBarTitle = stringResource(id = R.string.hourly_weather_tittle)
@@ -63,12 +81,22 @@ fun HourlyScreen(
                     onBack= onBack,
                 )
             },
+            floatingActionButton = {
+                FloatingActionButton(onClick = { viewModel.onRefreshClick() }) {
+                    Icon(
+                        imageVector = Icons.Default.Refresh,
+                        contentDescription = stringResource(id = R.string.back)
+                    )
+                }
+            },
+            snackbarHost = {
+                SnackbarHost(hostState = snackBarHostState)
+            },
             modifier = Modifier
                 .nestedScroll(scrollBehavior.nestedScrollConnection)
                 .background(MaterialTheme.colorScheme.inversePrimary),
             contentWindowInsets = WindowInsets.safeDrawing
         ) { padding ->
-            val state by viewModel.state.collectAsState()
 
             if (state.loading) {
                 LoadingProgressIndicator(modifier = Modifier.padding(padding))
